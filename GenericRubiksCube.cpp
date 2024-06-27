f8cd0df586d8c2c4f7261385bdc5c25b98157e69
MaanasaV <maanasa2005@gmail.com>
2024-06-27 11:38:35 +0530

    Added GenericRubiksCube Model

---
+++ GenericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maana on 27-06-2024.
+//
+
+#include "GenericRubiksCube.h"
---
+++ GenericRubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by maana on 27-06-2024.
+//
+
+#ifndef GENERICRUBIKSCUBE_H
+#define GENERICRUBIKSCUBE_H
+
+
+
+class GenericRubiksCube {
+
+};
+
+
+
+#endif //GENERICRUBIKSCUBE_H
